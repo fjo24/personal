db55fb8a9a2388a1a55d1e8f45a33b4d978611d4
Rafael Torrealba - Reta110 <rafaeltorrealba6@gmail.com>
2017-04-02 14:46:30 -0300

    show view

---
+++ resources/views/hr/personal/show.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.layout')
+
+@section('title', 'Crear usuario')
+
+
+
+@section('content')
+    {!! Form::open(['route' => 'hr.personal.store', 'method' => 'POST']) !!}
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box">
+                <div class="box-header with-border">
+                    @include('hr.partials.errors')
+                    <h3 class="box-title">Nombre: {{  }}</h3>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <!--Contenido-->
+                                <p>Nombre: </p>
+                                <p>Nombre: </p>
+                                <p>Nombre: </p>
+                                <p>Nombre: </p>
+                                <p>Nombre: </p>
+                                <p>Nombre: </p>
+                                <p>Nombre: </p>
+                                <p>Nombre: </p>
+                                <p>Nombre: </p>
+                                <p>Nombre: </p>
+                                <p>Nombre: </p>
+                                <p>Nombre: </p>
+                                <p>Nombre: </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="for">
+        {!! Form::submit('Registrar', ['class'=> 'btn btn-primary']) !!}
+        <a class="btn btn-danger" href="{{ route('hr.personal.index')}}">
+            Cancelar
+        </a>
+    </div>
+    {!! Form::close() !!}
+@endsection
+
+@section('js')
+    <script>
+        //datepicker
+
+        $('.datepicker').datepicker({
+            format: "dd-mm-yyyy",
+            language: "es",
+            autoclose: true
+        });
+
+        //chosen
+        $('.documento').chosen({
+            placeholder_text_single: "Tipo de documento"
+        });
+        $('.puesto').chosen({
+            placeholder_text_single: "Tipo de documento"
+        });
+
+        // phone mask
+
+        $(document).ready(function () {
+            $('.date').mask('00/00/0000');
+            $('.time').mask('00:00:00');
+            $('.date_time').mask('00/00/0000 00:00:00');
+            $('.cep').mask('00000-000');
+            $('.phone').mask('0000-0000');
+            $('.phone_with_ddd').mask('(00) 0000-0000');
+            $('.phone_us').mask('(000) 000-0000');
+            $('.mixed').mask('AAA 000-S0S');
+            $('.cpf').mask('000.000.000-00', {reverse: true});
+            $('.cnpj').mask('00.000.000/0000-00', {reverse: true});
+            $('.money').mask('000.000.000.000.000,00', {reverse: true});
+            $('.money2').mask("#.##0,00", {reverse: true});
+            $('.ip_address').mask('0ZZ.0ZZ.0ZZ.0ZZ', {
+                translation: {
+                    'Z': {
+                        pattern: /[0-9]/, optional: true
+                    }
+                }
+            });
+            $('.ip_address').mask('099.099.099.099');
+            $('.percent').mask('##0,00%', {reverse: true});
+            $('.clear-if-not-match').mask("00/00/0000", {clearIfNotMatch: true});
+            $('.phonemask').mask("(000)000000000", {placeholder: "(__)____"});
+            $('.fallback').mask("00r00r0000", {
+                translation: {
+                    'r': {
+                        pattern: /[\/]/,
+                        fallback: '/'
+                    },
+                    placeholder: "(__)____"
+                }
+            });
+            $('.selectonfocus').mask("00/00/0000", {selectOnFocus: true});
+        });
+    </script>
+@endsection
